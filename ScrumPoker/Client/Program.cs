43609777ef120fdac1f97944b030d4395c02fac1
Natan Martins <natan.martins20@hotmail.com>
2023-05-25 13:34:05 -0300

    Criado autenticação com o google via firebase

--- ScrumPoker/Client/Program.cs
+++ ScrumPoker/Client/Program.cs
@@ -8,4 +8,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.UserOptions.NameClaim = "given_name";
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
 await builder.Build().RunAsync();
